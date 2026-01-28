1e86713d026d8a6df898dce1fb5b8872e3a9e440
olive <olivemenorah@gmail.com>
2026-01-28 16:52:07 +0300

    Exercise 3.9

--- part3/the_phonebook_backend/index.js
+++ part3/the_phonebook_backend/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 var morgan = require("morgan");
+const cors = require("cors");
 
 morgan.token("body", (req) => JSON.stringify(req.body));
-
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
